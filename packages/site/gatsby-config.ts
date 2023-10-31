10e85bf32ff0e46b10cbb26c0a661a9d7d7da3fb
DM <dmoosocool@gmail.com>
2023-11-01 05:51:03 +0800

    chore: build

--- packages/site/gatsby-config.ts
+++ packages/site/gatsby-config.ts
@@ -1,4 +1,9 @@
 import { GatsbyConfig } from 'gatsby';
+import dotenv from 'dotenv';
+
+dotenv.config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
 
 const config: GatsbyConfig = {
   // This is required to make use of the React 17+ JSX transform.
@@ -9,6 +14,12 @@ const config: GatsbyConfig = {
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
     'gatsby-plugin-postcss',
+    {
+      resolve: `gatsby-plugin-env-variables`,
+      options: {
+        allowList: ['SNAP_ORIGIN', 'ENVIRONMENT'],
+      },
+    },
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
